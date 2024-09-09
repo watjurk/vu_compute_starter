d2cef06ec1d4a656a6fe81130750853ca45a5b9c
Wiktor Jurkiewicz <watjurk@gmail.com>
2024-09-09 17:41:46 +0200

    init

---
+++ ollama/start.sh
@@ -0,0 +1,4 @@
+HOME=/local/data/wju203/O_HOME
+
+
+./ollama/bin/ollama serve
---
+++ wisper/install.sh
@@ -0,0 +1,4 @@
+HOME=/local/data/wju203/PIP_HOME
+
+pip install torch
+pip install nvidia-cublas-cu11 nvidia-cudnn-cu11
