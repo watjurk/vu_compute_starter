901687bdf94f557ad97e6d6a053ffb9cbb2af7e7
Wiktor Jurkiewicz <watjurk@gmail.com>
2024-09-10 12:21:14 +0200

    update wisper install

--- wisper/install.sh
+++ wisper/install.sh
@@ -2,3 +2,4 @@ HOME=/local/data/wju203/PIP_HOME
 
 pip install torch
 pip install nvidia-cublas-cu11 nvidia-cudnn-cu11
+pip install uvicorn
--- wisper/start.sh
+++ wisper/start.sh
@@ -1,3 +1,3 @@
 HOME=/local/data/wju203/PIP_HOME
 
-LD_LIBRARY_PATH="/local/data/wju203/PIP_HOME/.local/lib/python3.12/site-packages/torch/lib/:/local/data/wju203/PIP_HOME/.local/lib/python3.12/site-packages/nvidia/cudnn/lib/:/local/data/wju203/lib/" UVICORN_PORT=8085 uvicorn faster_whisper_server.main:app
+LD_LIBRARY_PATH="/local/data/wju203/PIP_HOME/.local/lib/python3.12/site-packages/torch/lib/:/local/data/wju203/PIP_HOME/.local/lib/python3.12/site-packages/nvidia/cudnn/lib/:/local/data/wju203/lib/" UVICORN_PORT=8085 python -m uvicorn faster_whisper_server.main:app
